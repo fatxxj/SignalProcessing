a928115e545544b51ae6c320360e4bbc79294dff
Fat Halimi <halimifat@gmail.com>
2022-03-15 01:13:58 +0100

    second commit

---
+++ A2_OsnovniSignali/OsnovniSignali.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as pt
+
+vreme= range(-2,5)
+x = [-1,2,-3,0,2,3,-1]
+plt.plot(vreme,x)
+plt.show()
---
+++ A5_DFT/DFT_vezba.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cmath
+from scipy.fft import fft, ifft
+
+N=64 # dolzina na niza
+n=np.asarray(list(range(0,N)))
+Omega= (2.0*np.pi / 64)*2
+Fi=np.pi/6
+x=np.zeros(N)
+for i in range (N):
+ #   x[i]=np.cos(Omega*i*Fi)
+    x[i]=2*np.cos(Omega*i*Fi)+np.sin(Omega*3*i)+np.cos(Omega*5*i+np.pi/3)
+
+    print(x)
+    plt.figure()
+    plt.stem(n,x,use_line_collection=True)
+    plt.xlabel('n')
+    plt.ylabel('x[n]')
+    plt.title("Izgled na singlaot")
+    plt.show()
+
+ # Presmetka na amplituda i faza
+
+ Faza = np.zeros(N)
